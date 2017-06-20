8ac5737a8afc4774363a4a254a97cd8e1bcc0331
diydyq <diydyq@126.com>
2017-06-20 14:28:20 +0800

    Remove arguments.callee

--- chai.js
+++ chai.js
@@ -9191,7 +9191,7 @@ function AssertionError (message, _props, ssf) {
   }
 
   // capture stack trace
-  ssf = ssf || arguments.callee;
+  // ssf = ssf || arguments.callee;
   if (ssf && Error.captureStackTrace) {
     Error.captureStackTrace(this, ssf);
   } else {
